30710320887a8a0ee3661d1dfbd3c4edcdfbcd46
Hernan Clich <hernan.clich@wolox.com.ar>
2021-06-24 17:09:25 -0300

    Initial setup

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Home from './screens/Home.svelte';
+</script>
+
+<Home />
---
+++ src/screens/Home.svelte
@@ -0,0 +1,29 @@
+<main>
+  <h1>Hello world!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutoriasl</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
+
+<style lang="scss">
+  main {
+    margin: 0 auto;
+    max-width: 240px;
+    padding: 1em;
+    text-align: center;
+
+    h1 {
+      color: #ff3e00;
+      font-size: 4em;
+      font-weight: 100;
+      text-transform: uppercase;
+    }
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
